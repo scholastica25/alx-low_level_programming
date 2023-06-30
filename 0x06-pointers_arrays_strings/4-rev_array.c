a04b03260801d9a7ac13437f3a4f370cba036143
scholastica25 <schogbonnamara@gmail.com>
2023-06-30 16:06:47 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,14 @@
  * Return: void
  */
 void reverse_array(int *a, int n)
-{       
-        int i;
-        int t;
-        
-        for (i = 0; i < n++; n++)
-        {       
-                t = a[i];
-                a[i] a[n];
-		a[n] =t;
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
 }
