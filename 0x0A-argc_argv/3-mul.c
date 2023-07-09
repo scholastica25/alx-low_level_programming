af5b19e92e35bed594fac5147641af6a1a75140c
scholastica25 <schogbonnamara@gmail.com>
2023-07-09 04:52:57 -0700

    updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -24,7 +24,7 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] == '0' && s[i] == '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
@@ -32,7 +32,7 @@ int _atoi(char *s)
 			n = n * 10 + digit;
 			f = 1;
 		if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
+			break;
 		f = 0;
 		}
 		i++;
