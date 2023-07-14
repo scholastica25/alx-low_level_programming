90e8468d6a586bcb6a8deff823cdc70f058b06a2
scholastica25 <schogbonnamara@gmail.com>
2023-07-13 18:43:38 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
- * Return: a poiter to the allocated memory
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
