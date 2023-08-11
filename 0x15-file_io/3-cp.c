ee20d251bb1ec81fabdc615df573e03ed7d444a2
scholastica25 <schogbonnamara@gmail.com>
2023-08-11 09:04:05 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "usage: cp file_from File_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from File_to\n");
 		exit(97);
 	}
 	buffer = create_buffer(argv[2]);
